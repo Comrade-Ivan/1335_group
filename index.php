ef27276392ec7614e14db6202fef0c6cd545a300
Comrade-Ivan <1335308@gmail.com>
2020-10-27 19:58:02 +0300

    27/10/2020

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+  <head> <!--Информация для браузера-->
+    <meta charset="UTF-8">
+    <title>Страница</title>
+  </head>
+  <body> <!--Видимая часть страницы+-->
+    <h1>Моя первая страница</h1> <!--Заголовое первого уровня (header 1) *тэг h1 на странице может быть только один-->
+    <div>
+      <h2>Тэги для текста</h2> <!--h1 - h6-->
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero, iure, provident, earum, voluptates nihil recusandae distinctio odit adipisci at voluptate ea eaque dolorem rerum quidem accusamus ipsum necessitatibus expedita id illum maiores? Sunt, accusantium, mollitia odit sequi doloremque odio deserunt veritatis eius ipsa assumenda ratione vitae quod totam voluptates a possimus officiis delectus quas voluptatibus et <br> commodi nostrum quae <span style="color:blue;">laudantium</span> minus incidunt autem id impedit est eaque molestias rerum amet eos quasi? Ad repellat in maiores similique consequatur neque at <span>unde rerum totam quam</span>. Eius, illum harum fugit quam pariatur accusamus deserunt necessitatibus alias tempore sit saepe nam sequi natus eos placeat ab aliquid nisi quisquam eligendi ipsam aut a velit modi amet rem. Consequatur, vero, minus, facere consequuntur dolores qui quae natus eos omnis eum blanditiis quia ea sapiente ad necessitatibus quo nobis dolorum cum. Itaque, consequatur, voluptas, id, provident illum doloribus maiores expedita nulla voluptatibus corrupti at asperiores.</p> <!--Параграф-->
+      <!--<b></b> <i></i>-->
+      <span>Lorem ipsum dolor.</span> <!--Строчный тэг для текста без изначального оформления-->
+      <span>Lorem ipsum dolor.</span>
+      <span>Lorem ipsum dolor.</span>
+      <p>Lorem ipsum dolor sit amet.</p>
+    </div>
+    <div>
+      <h2>Ссылки</h2>
+      <a href="https://yandex.ru" target="_blank">Абсолютная</a> <!--Cсылка--> 
+      <a href="octopus.php">Относительная</a> <!--Cсылка--> 
+    </div>
+    <div>
+      <h2>Изображения</h2>
+      <img src="https://cdn.custom-cursor.com/cursors/elmo_rise_meme_945.png" alt="Елмо" width="300"> <!--Картинка-->
+      <img src="img/cookie-monster.jpg" alt="" width="300">
+    </div>
+    <div>
+      <h2>Списки</h2>
+      <ol> <!--Нумерованный список - ordered list-->
+        <li>Элемент списка</li> <!--List item - элемент списка-->
+        <li>Элемент списка</li>
+        <li>Элемент списка</li>
+        <li>Элемент списка</li>
+      </ol>
+      <ul> <!--Ненумерованный список - unordered list-->
+        <li>Элемент списка</li>
+        <li>Элемент списка</li>
+        <li>Элемент списка</li>
+        <li><a href="https://vk.com">Вконтакте</a></li>
+      </ul>
+    </div>
+    <!--Блочные тэги - div p h1-h6 ol ul li table form-->
+    <!--Строчные тэги - a img span input-->
+  </body>
+</html>
