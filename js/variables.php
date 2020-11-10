798540b4c1a0c3b79bbcc3dcb340682c4146027d
Comrade-Ivan <1335308@gmail.com>
2020-11-10 16:38:53 +0300

    10/11/2020

---
+++ js/variables.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="ru">
+<head>
+  <meta charset="UTF-8">
+  <title>Переменные в JS</title>
+</head>
+<body>
+  <h1>Переменные и типы данных</h1>
+  <script>
+    //alert("Привет, мир!");
+    /*
+      Многострочный 
+      комментарий
+    */
+  </script>
+  <script src="variables.js"></script>
+</body>
+</html>
